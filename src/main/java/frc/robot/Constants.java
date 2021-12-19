317cb93e1d8d68ad5483c3f0b133a63ab88442e2
Programmers <broncobots@gmail.com>
2021-12-18 19:05:13 -0600

    Initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    /**
+     * The left-to-right distance between the drivetrain wheels
+     *
+     * Should be measured from center to center.
+     */
+    public static final double DRIVETRAIN_TRACKWIDTH_METERS = Units.inchesToMeters(17.5);
+    /**
+     * The front-to-back distance between the drivetrain wheels.
+     *
+     * Should be measured from center to center.
+     */
+    public static final double DRIVETRAIN_WHEELBASE_METERS = Units.inchesToMeters(17.5);
+
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 4;
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 3;
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 11;
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(102.39);
+
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2;
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 1;
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 12;
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(61.69);
+
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 6;
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 5;
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 10;
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(2.64);
+
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 8;
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 7;
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 9;
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(309.28);
+
+    public static final double FALCON_MAX_RPM = 6380.0;
+    public static final int DRIVER_CONTROLLER_PORT = 0;
+}
